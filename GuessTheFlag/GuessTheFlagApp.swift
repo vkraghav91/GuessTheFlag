65da80dbcbd555ae48d3a473a0e40e5a60fb8e3b
Varun Kumar Raghav <vrnrghv@gmail.com>
2021-08-02 17:12:09 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Varun Kumar Raghav on 02/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
